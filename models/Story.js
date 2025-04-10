4764f7dba753776dadac530fea2f2f34f784cec6
Sam007-dot <shivamkumbar44@gmail.com>
2025-04-10 19:27:51 +0530

    story debugged

--- models/Story.js
+++ models/Story.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const storySchema = new mongoose.Schema({
   storyName: { type: String, required: true },
-  storyContent: { type: Number, required: true },
+  storyContent: { type: String, required: true },
   storyBanner: { type: String, required: true } // Image path
 });
 
