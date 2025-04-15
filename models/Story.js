9d2348cc6623d5e9f891abe039d955215c4cf699
Sam007-dot <shivamkumbar44@gmail.com>
2025-04-15 15:51:40 +0530

    user with story

--- models/Story.js
+++ models/Story.js
@@ -1,22 +1,12 @@
-const mongoose = require("mongoose");
+// models/Story.js
+const mongoose = require('mongoose');
 
 const storySchema = new mongoose.Schema({
-  storyName: { type: String, required: true },
-  storyContent: { type: String, required: true },
-  storyBanner: { type: String },  // Image path (optional)
+  storyName: String,
+  storyContent: String,
+  storyBanner: String,
   createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  categories: [{ type: String }],
-  tags: [{ type: String }],
-  ratings: [{
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    rating: { type: Number, min: 1, max: 5 }
-  }],
-  reviews: [{
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    reviewText: { type: String }
-  }]
 });
 
-module.exports = mongoose.model("Story", storySchema);
+module.exports = mongoose.model('Story', storySchema);
