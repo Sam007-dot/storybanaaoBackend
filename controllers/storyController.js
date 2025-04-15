1f4294757aaaac2365a19095acbba40cdbecb9c8
Sam007-dot <shivamkumbar44@gmail.com>
2025-04-15 11:18:25 +0530

    added new things

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,4 +1,5 @@
 const Story = require("../models/Story");
+const User = require("../models/User")
 
 // 📥 Add a new story
 exports.addStory = async (req, res) => {
@@ -6,7 +7,8 @@ exports.addStory = async (req, res) => {
     const newStory = new Story({
       storyName: req.body.storyName,
       storyContent: req.body.storyContent,
-      storyBanner: req.file?.path, // Store file path if uploaded
+      storyBanner: req.file?.path || '', // Store file path if uploaded
+      author: req.user._id, // coming from JWT/session
     });
 
     await newStory.save();
@@ -19,7 +21,7 @@ exports.addStory = async (req, res) => {
 // 📄 Get all storys
 exports.getStories = async (req, res) => {
   try {
-    const stories = await Story.find();
+    const stories = await Story.find().populate('author', 'username');
     res.json(stories);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -29,13 +31,11 @@ exports.getStories = async (req, res) => {
 // 🔎 Get single story by ID
 exports.getStoryById = async (req, res) => {
   try {
-    const story = await Story.findById(req.params.id);
-
-    if (!story) {
-      return res.status(404).json({ message: "Story not found" });
-    }
-
-    res.json(story);
+    const user = await User.findOne({ username: req.params.username });
+    if (!user) return res.status(404).json({ message: "User not found" });
+  
+    const stories = await Story.find({ author: user._id });
+    res.json(stories);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
